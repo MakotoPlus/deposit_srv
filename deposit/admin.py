4c0ddb8f63a9f89e510b757c2884ba15bca79b2d
makoto <fukupeemail@gmail.com>
2021-11-27 22:57:11 +0900

    DataBase項目構築

--- deposit/admin.py
+++ deposit/admin.py
@@ -8,3 +8,28 @@ class AdminTm_Tm_DepositGroup(admin.ModelAdmin):
     fields = ('deposit_group_name', 'order_dsp', 'delete_flag', 'update_date', )
     list_display = ('deposit_group_key', 'deposit_group_name', 'order_dsp', 'delete_flag', 'update_date', )
     ordering = ('deposit_group_key', )
+
+
+@admin.register(models.Tm_DepositItem)
+class AdminTm_Tm_DepositItem(admin.ModelAdmin):
+    fields = ('depositItem_name', 'deposit_group_key', 'moneyType_key', 'savings_flag', 'order_dsp', 'delete_flag', 'update_date',  )
+    list_display = ('depositItem_key', 'depositItem_name', 'deposit_group_key', 'moneyType_key', 'savings_flag', 'order_dsp', 'delete_flag', 'update_date',  )
+    ordering = ('depositItem_key', )
+
+@admin.register(models.Tm_MoneyType)
+class AdminTm_Tm_MoneyType(admin.ModelAdmin):
+    fields = ('moneyType_name', 'delete_flag', 'update_date',  )
+    list_display = ('moneyType_key', 'moneyType_name', 'delete_flag', 'update_date',  )
+    ordering = ('moneyType_key', )
+
+@admin.register(models.Tt_Savings)
+class AdminTm_Tt_Savings(admin.ModelAdmin):
+    fields = ('depositItem_key', 'order_dsp', 'deposit_type', 'deposit_value', 'delete_flag', 'update_date',  )
+    list_display = ('savings_key', 'depositItem_key', 'order_dsp', 'deposit_type', 'deposit_value', 'delete_flag', 'update_date',  )
+    ordering = ('savings_key', )
+
+@admin.register(models.Tt_Deposit)
+class AdminTm_Tt_Deposit(admin.ModelAdmin):
+    fields = ('depositItem_key', 'deposit_type', 'deposit_value', 'insert_yyyymmdd', 'delete_flag', 'update_date',  'memo', )
+    list_display = ('deposit_key', 'depositItem_key', 'deposit_type', 'deposit_value', 'insert_yyyymmdd', 'delete_flag', 'update_date',  'memo', )
+    ordering = ('deposit_key', )
--- deposit/models.py
+++ deposit/models.py
@@ -1,7 +1,9 @@
 from django.contrib.auth import get_user_model
 from django.db import models
 from deposit.model_base import ModelBase
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
 # Create your models here.
 #預金項目グループ
 class Tm_DepositGroup(ModelBase):
@@ -19,6 +21,82 @@ class Tm_DepositGroup(ModelBase):
     order_dsp = models.IntegerField(verbose_name='表示順序')
     delete_flag = models.BooleanField(verbose_name='削除フラグ')
     update_date = models.DateTimeField(verbose_name='更新日時')
-    # u_user = models.ForeignKey(User, on_delete=models.PROTECT, related_name='depositgroup_u_user', verbose_name='更新者')
+    #u_user = models.ForeignKey(User, on_delete=models.PROTECT, related_name='depositgroup_u_user', verbose_name='更新者')
     def __str__(self):
         return self.deposit_group_name
+
+#金種
+class Tm_MoneyType(ModelBase):
+    class Meta:
+        db_table = 'Tm_MoneyType'
+        verbose_name = '金種(Tm_MoneyType)'
+        verbose_name_plural = '金種(Tm_MoneyType)'
+        unique_together = (
+            ('moneyType_name',),
+        )
+
+    moneyType_key = models.AutoField(primary_key=True, verbose_name='金種ID')
+    moneyType_name = models.CharField(max_length=40, verbose_name='金種名')
+    delete_flag = models.BooleanField(verbose_name='削除フラグ')
+    update_date = models.DateTimeField(verbose_name='更新日時')
+    #u_user = models.ForeignKey(User, on_delete=models.PROTECT, related_name='moneytype_u_user', verbose_name='更新者')
+    def __str__(self):
+        return self.moneyType_name
+
+
+#預金項目
+class Tm_DepositItem(ModelBase):
+    class Meta:
+        db_table = 'Tm_DepositItem'
+        verbose_name = '預金項目(Tm_DepositItem)'
+        verbose_name_plural = '預金項目(Tm_DepositItem)'
+        unique_together = (
+            ('depositItem_name',),
+        )
+        ordering = ('order_dsp',)
+
+    depositItem_key = models.AutoField(primary_key=True, verbose_name='預金項目ID')
+    depositItem_name = models.CharField(max_length=40, verbose_name='預金項目名')
+    deposit_group_key = models.ForeignKey(Tm_DepositGroup, on_delete=models.PROTECT, related_name='deposititem_deposit_group_key', verbose_name='預金項目グループID')
+    moneyType_key = models.ForeignKey(Tm_MoneyType, on_delete=models.PROTECT, related_name='deposititem_moneytype_key', verbose_name='金種ID')
+    savings_flag = models.BooleanField(verbose_name='積立項目フラグ')
+    order_dsp = models.IntegerField(verbose_name='表示順序')
+    delete_flag = models.BooleanField(verbose_name='削除フラグ')
+    update_date = models.DateTimeField(verbose_name='更新日時')
+    #u_user = models.ForeignKey(User, on_delete=models.PROTECT, related_name='deposititem_u_user', verbose_name='更新者')
+    def __str__(self):
+        return self.depositItem_name
+
+
+#貯金設定
+class Tt_Savings(ModelBase):
+    class Meta:
+        db_table = 'Tt_Savings'
+        verbose_name = '貯金設定(Tt_Savings)'
+        verbose_name_plural = '貯金設定(Tt_Savings)'
+
+    savings_key = models.AutoField(primary_key=True, verbose_name='貯金設定ID')
+    depositItem_key = models.ForeignKey(Tm_DepositItem, on_delete=models.PROTECT, related_name='savings_deposititem_key', verbose_name='預金項目ID')
+    order_dsp = models.IntegerField(verbose_name='表示順序')
+    deposit_type = models.IntegerField(verbose_name='貯金タイプ')
+    deposit_value = models.IntegerField(verbose_name='金額')
+    delete_flag = models.BooleanField(verbose_name='削除フラグ')
+    update_date = models.DateTimeField(verbose_name='更新日時')
+    #u_user = models.ForeignKey(User, on_delete=models.PROTECT, related_name='savings_u_user', verbose_name='更新者')
+
+#預金トラン
+class Tt_Deposit(ModelBase):
+    class Meta:
+        db_table = 'Tt_Deposit'
+        verbose_name = '預金トラン(Tt_Deposit)'
+        verbose_name_plural = '預金トラン(Tt_Deposit)'
+
+    deposit_key = models.AutoField(primary_key=True, verbose_name='預金ID')
+    depositItem_key = models.ForeignKey(Tm_DepositItem, on_delete=models.PROTECT, related_name='deposit_deposititem_key', verbose_name='預金項目ID')
+    deposit_type = models.IntegerField(verbose_name='貯金タイプ')
+    deposit_value = models.IntegerField(verbose_name='金額')
+    insert_yyyymmdd = models.CharField(max_length=10, verbose_name='登録年月日')
+    delete_flag = models.BooleanField(verbose_name='削除フラグ')
+    update_date = models.DateTimeField(verbose_name='更新日時')
+    #u_user = models.ForeignKey(User, on_delete=models.PROTECT, related_name='deposit_u_user', verbose_name='更新者')
+    memo = models.CharField(null=True, max_length=1024, verbose_name='補足')
